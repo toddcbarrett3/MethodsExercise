318059491197c98c747f52cfbdd7fbd9896e5875
Todd Barrett <toddcbarrett3@gmail.com>
2023-06-19 08:52:51 -0700

    Completed Excersise 1

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("What id your favorite activity?");
+            string activity = Console.ReadLine();
+            Console.WriteLine("What is your favorite food?");
+            string food = Console.ReadLine();
+            Console.WriteLine("Where is your favorite place to be?");
+            string place = Console.ReadLine();
+
+            Console.WriteLine($"{name} loves to spend time in {place}.  When {name} is in {place} they like to spend time {activity}.  When {name} gets hungry they like to eat {food}.");
+
         }
     }
 }
