5c10557fddfd61dc74e0da00d2a29b98485a6962
Todd Barrett <toddcbarrett3@gmail.com>
2023-06-19 11:54:21 -0700

    Completed Exercise 2

---
+++ MethodsExercise/Method1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsExercise
+{
+    internal class Method1
+    {
+        public static void Add(int num1, int num2)
+        {
+        int result = num1 + num2;
+        Console.WriteLine(result);
+        }
+        public static void Subtract(int num1, int num2)
+        {
+            int result = num1 - num2;
+            Console.WriteLine(result);
+        }
+        public static void Multiply(int num1, int num2)
+        {
+            int result = num1 * num2;
+            Console.WriteLine(result);
+
+        }
+        public static void Divide(double num1, double num2)
+        {
+            double result = num1 / num2;
+            Console.WriteLine(result);
+        }
+    }
+}
+
+
--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,6 +4,8 @@
     {
         static void Main(string[] args)
         {
+            //Exercise 1:
+
             Console.WriteLine("What is your name?");
             string name = Console.ReadLine();
             Console.WriteLine("What id your favorite activity?");
@@ -15,6 +17,38 @@
 
             Console.WriteLine($"{name} loves to spend time in {place}.  When {name} is in {place} they like to spend time {activity}.  When {name} gets hungry they like to eat {food}.");
 
+
+            //Exercise 2: (Methods are on seraparate tab)
+
+            Console.WriteLine("Do you want to \"Add\", \"Subtract\", \"Multiply\", or \"Divide\" 2 numbers?");
+            string math = Console.ReadLine();
+
+            Console.WriteLine("First Number?");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Second Number?");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            
+            if(math == "Add") 
+            {
+                Method1.Add(num1, num2);
+            }
+            else if (math == "Subtract")
+            {
+                Method1.Subtract(num1, num2);
+            }
+            else if (math == "Multiply")
+            {
+                Method1.Multiply(num1, num2);
+            }
+            else if (math == "Divide")
+            {
+                Method1.Divide(num1, num2);
+            }
+            else
+            {
+                Console.WriteLine("Try Again - Make Sure to Capitalize the First Letter!");
+            }
         }
     }
 }
